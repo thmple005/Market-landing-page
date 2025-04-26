c9397e1695e625c686714281fa84b78c87dcb163
Shivam Tiwari <110393597+shivamtiwari151198@users.noreply.github.com>
2025-04-26 14:29:25 +0530

    Add and fix bug

--- app/api/sign-up/route.ts
+++ app/api/sign-up/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import pool from "@/lib";
 
 export async function POST(req: NextRequest) {
